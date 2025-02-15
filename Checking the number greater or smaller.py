b6188b6d7af30a51359475e920be6f737e5c7657
aaravhajare <hajareaarav66@gmail.com>
2025-02-15 11:17:11 +0530

    print (f"num1 is {num1}")

---
+++ Checking the number greater or smaller.py
@@ -0,0 +1,15 @@
+
+num1 = int(input("enter number"))
+num2 = int(input("enter a number"))
+
+print (f"num1 is {num1}")
+print (f"num2 is {num2}")
+
+if num1 > num2 :
+    print(f"the {num1} is bigger")
+    print("I am in if block")
+
+else :
+    print(f"the {num2} is bigger")
+
+print("I am i else block")
